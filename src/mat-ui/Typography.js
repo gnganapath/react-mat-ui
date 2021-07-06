6dc6b4fb5ce6135c64ae2df4b302201ed277c736
gnganapath <gnganapathi@gmail.com>
2021-07-06 08:47:34 +0530

    1 file modified

--- src/mat-ui/Typography.js
+++ src/mat-ui/Typography.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+// or
+//destructure make less performance issue
+//import { Typography } from '@material-ui/core';
 
-const Typography = () => {
+const TypographyUI = () => {
   return (
     <>
-      <h5>Typography</h5>
+      <h5>Typography UI</h5>
+      <Typography variant="h1" component="h2" gutterBottom>
+        h1. Heading
+      </Typography>
     </>
   );
 };
 
-export default Typography;
+export default TypographyUI;
